9b133a54022b075a54af21c6682f7cdea0eab3da
ChloeP-1038 <pettitc1038@student.faytechcc.edu>
2025-11-04 23:08:03 -0500

    Add files via upload

---
+++ P1LAB1_PettitChloe.py
@@ -0,0 +1,8 @@
+# Chloe Pettit
+# 11/04/2025
+# P1Lab1
+# Teaching students how to create a Python Program.
+
+first_name = input('Enter your first name')
+last_name = input('Enter your last name')
+print('Hello!', first_name, last_name, 'Welcome to the course CTI-110')
